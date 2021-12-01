a3081f5b2f32715e6427f4a42c1b46cc2a5bb723
medev21 <benmarteen07@gmail.com>
2021-11-30 22:02:02 -0500

    add pre commit yaml

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+
 # from typing import Optional
 
 app = FastAPI()
@@ -11,15 +12,14 @@ def api_status():
 
 @app.post("/api/board/{title}")
 def create_board(title: str):
-    return 'hello'
+    return "hello"
 
 
 @app.post("/api/list/{board_id}/{title}")
 def create_list(title: str, board_id: int):
-    return 'hello'
+    return "hello"
 
 
 @app.post("/api/card/{list_id}/{title}")
 def create_card(title: str, list_id: int):
-    return 'hello world'
-
+    return "hello world"
