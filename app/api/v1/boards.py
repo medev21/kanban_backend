86790c183b0ba7a6bc781830573fa2dc27cedd21
medev21 <benmarteen07@gmail.com>
2021-12-29 21:33:31 -0500

    add create method and typing

--- app/api/v1/boards.py
+++ app/api/v1/boards.py
@@ -1,16 +1,21 @@
-from typing import List
-from fastapi import APIRouter
-from app.database.boards import fetch_boards
+from typing import Dict, List
+from fastapi import APIRouter, HTTPException
+from app.database.boards import add_board, fetch_boards
+from app.models.board import Board
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", response_description="List of boards!")
 async def get_boards() -> List:
     boards = await fetch_boards()
     return boards
 
 
-@router.post("/{title}")
-def create_board(title: str):
-    return "hello"
+@router.post("/create", response_description="Board Created!")
+async def create_board(board: Board) -> Dict:
+    board_data = board.dict()
+    response = await add_board(board_data)
+    if response:
+        return response
+    raise HTTPException(400, "Something went wrong creating the board")
--- app/database/boards.py
+++ app/database/boards.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import Dict, List
 from .session import database
 from .db_helpers import board_helper
 
 board_collection = database.boardstest
 
 
-async def fetch_boards() -> List:
+async def fetch_boards() -> List[Dict[str, str]]:
     boards_list = []
     boards_result = board_collection.find()
 
@@ -13,3 +13,9 @@ async def fetch_boards() -> List:
         boards_list.append(board_helper(board))
 
     return boards_list
+
+
+async def add_board(board: dict) -> Dict[str, str]:
+    insert_response = await board_collection.insert_one(board)
+    new_board_dict = await board_collection.find_one(insert_response.inserted_id)
+    return board_helper(new_board_dict)
