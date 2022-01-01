31dd707e63ac55780e588e23d5f0e38844bc2009
medev21 <benmarteen07@gmail.com>
2022-01-01 00:29:42 -0500

    move set_object_id func to db_helpers

--- app/database/db_helpers.py
+++ app/database/db_helpers.py
@@ -1,5 +1,22 @@
+from bson import ObjectId
+
+
 def board_helper(board: dict) -> dict:
     return {
         "id": str(board["_id"]),
         "title": board["title"],
     }
+
+
+# TODO: ADD TYPING IN THIS FUNCTION
+async def find_by_object_id(collection, id_param):
+    response = collection.find_one({"_id": ObjectId(id_param)})
+    return response
+
+
+def set_object_id(id_param: str) -> ObjectId:
+    try:
+        object_id = ObjectId(id_param)
+        return object_id
+    except Exception:
+        raise TypeError("ID is not valid!")
