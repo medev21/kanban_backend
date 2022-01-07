744a40625ef6670aaad7574b7c0a6bde88bfbc88
medev21 <benmarteen07@gmail.com>
2022-01-07 00:09:43 -0500

    add edit_category method for mongo update

--- app/database/category.py
+++ app/database/category.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, Union
 from .session import database
 from .db_helpers import response_helper, set_object_id
 
@@ -6,7 +6,7 @@ from .db_helpers import response_helper, set_object_id
 category_collection = database.liststest
 
 
-async def insert_category(category: dict) -> Dict:
+async def insert_category(category: dict) -> Union[Dict, bool]:
     insert_response = await category_collection.insert_one(category)
     if insert_response.acknowledged:
         new_cat_dict = await category_collection.find_one(insert_response.inserted_id)
@@ -16,4 +16,21 @@ async def insert_category(category: dict) -> Dict:
             status=200,
         )
         return response_dict
+    return False
+
+
+async def edit_category(category: dict) -> Union[Dict, bool]:
+    category_object_id = set_object_id(category["id"])
+    update_response = await category_collection.update_one(
+        {"_id": category_object_id},
+        {"$set": {"title": category["title"]}},
+    )
+    if update_response.modified_count:
+        modified_board = await category_collection.find_one({"_id": category_object_id})
+        response_obj = dict(
+            data=response_helper(modified_board),
+            message="Category updated!",
+            status=200,
+        )
+        return response_obj
     return False
