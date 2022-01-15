f92a2e7bd6eb2eadbce514791d5478ea213d1993
medev21 <benmarteen07@gmail.com>
2022-01-15 08:03:25 -0500

    add update route for category

--- app/api/v1/category.py
+++ app/api/v1/category.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
-from app.models.base_class import ErrorResponseModel, ResponseModel
-from app.models.category import Category
-from app.database.category import insert_category
+from app.models.base_class import ResponseModel
+from app.models.category import Category, UpdateCategory
+from app.database.category import insert_category, edit_category
 
 router = APIRouter()
 
@@ -16,3 +16,13 @@ async def create_category(category: Category):
     if response:
         return response
     raise HTTPException(500, "Something went wrong!")
+
+
+@router.put(
+    "/update", response_model=ResponseModel, response_description="Category Updated!"
+)
+async def update_category(category: UpdateCategory):
+    response = await edit_category(category.dict())
+    if response:
+        return response
+    raise HTTPException(404, "There is no category to update!")
